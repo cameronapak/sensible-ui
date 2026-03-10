5111e3d4911f39ae7634498ec70edc1e54ed93d7
Cameron Pak <cameronandrewpak@gmail.com>
2026-03-10 03:33:32 -0500

    Add demo page and move input styles
    Introduces a new demo page (`screenshots-page.html`) to showcase
    component styling. The CSS styles for form inputs, which were previously
    in `backup.css`, have been moved to a dedicated `input.css` file for
    better organization and clarity. An `image.css` file is also added to
    style images and a `utils.css` for general utility classes like
    `.container`.

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import homepage from './index.html'
+import demo from './screenshots-page.html'
 
 const server = Bun.serve({
   routes: {
     '/': homepage,
+    '/demo': demo,
   },
   development: {
     hmr: true,
